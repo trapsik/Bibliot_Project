1ad236dcfa2d17001faa7ef786220b43c953cd56
Chris.Moon <160264916+fckapp@users.noreply.github.com>
2024-08-22 11:13:43 +0900

    Delete toggle.js

--- toggle.js
+++
@@ -1,8 +0,0 @@
-const togglebtn = document.querySelector('.navbar-togglebtn');
-const menu = document.querySelector('.navbar-menu');
-const aut = document.querySelector('.navbar-aut')
-
-togglebtn.addEventListener('click', () => {
-    menu.classList.toggle('active')
-    aut.classList.toggle('active')
-})
